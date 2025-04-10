4ab524ff10705c797ea49e59508f1d0638549594
Ihor Kobzar <supermensy@gmail.com>
2025-04-10 15:40:33 +0300

    Add mapper from csv to entity for Cab

---
+++ TestTaskDevelopsToday/Mapping/CabCsvMap.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+using TestTaskDevelopsToday.Entities;
+
+namespace TestTaskDevelopsToday.Mapping;
+
+public sealed class CabCsvMap : ClassMap<CabCsv>
+{
+    public CabCsvMap()
+    {
+        Map(m => m.TpepPickupDatetime).Name("tpep_pickup_datetime");
+        Map(m => m.TpepDropoffDatetime).Name("tpep_dropoff_datetime");
+        Map(m => m.PassengerCount).Name("passenger_count").Default(default(byte?));
+        Map(m => m.TripDistance).Name("trip_distance");
+        Map(m => m.StoreAndFwdFlag).Name("store_and_fwd_flag").Default(default(char?)!);
+        Map(m => m.PuLocationId).Name("PULocationID");
+        Map(m => m.DoLocationId).Name("DOLocationID");
+        Map(m => m.FareAmount).Name("fare_amount");
+        Map(m => m.TipAmount).Name("tip_amount");
+    }
+}
