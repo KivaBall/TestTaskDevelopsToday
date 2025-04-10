11677e767c65340e2b6df4fb781df8fe4000a37c
Ihor Kobzar <supermensy@gmail.com>
2025-04-10 15:40:16 +0300

    Add entity

---
+++ TestTaskDevelopsToday/Entities/CabCsv.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace TestTaskDevelopsToday.Entities;
+
+public sealed class CabCsv
+{
+    public DateTime TpepPickupDatetime { get; init; }
+    public DateTime TpepDropoffDatetime { get; init; }
+    public byte? PassengerCount { get; init; }
+    public decimal TripDistance { get; init; }
+    public string? StoreAndFwdFlag { get; init; }
+    public ushort PuLocationId { get; init; }
+    public ushort DoLocationId { get; init; }
+    public decimal FareAmount { get; init; }
+    public decimal TipAmount { get; init; }
+
+    public override string ToString()
+    {
+        return
+            $"TpepPickupDatetime: {TpepPickupDatetime}, " +
+            $"TpepDropoffDatetime: {TpepDropoffDatetime}, " +
+            $"PassengerCount: {PassengerCount?.ToString(CultureInfo.InvariantCulture) ?? "|empty|"}, " +
+            $"TripDistance: {TripDistance}, " +
+            $"StoreAndFwdFlag: {StoreAndFwdFlag?.ToString(CultureInfo.InvariantCulture) ?? "|empty|"}, " +
+            $"PuLocationId: {PuLocationId}, " +
+            $"DoLocationId: {DoLocationId}, " +
+            $"FareAmount: {FareAmount}, " +
+            $"TipAmount: {TipAmount}";
+    }
+}
